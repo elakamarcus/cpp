6cdb5b9335d9ff6d395f40c9d5182f105981b6d4
elakamarcus <n0m4d@hotmail.com>
2024-10-16 15:19:45 +0200

    remove old things

--- api_fortisiem_00.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-
-import sys, base64, requests
-
-def authit(url, user, password):
-    url = "https://" + serverAddr + "/phoenix/rest/"
-    auth = "Basic " + base64.b64encode(user + ":" + password)
-
